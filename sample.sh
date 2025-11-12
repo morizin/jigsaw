5e7813cdebe9c07adc715488e00bba52f24eba6a
Mohammed Rizin <mrizin2013@gmail.com>
2025-11-12 22:14:34 +0530

    workflow fix

--- sample.sh
+++ sample.sh
@@ -1,3 +1,6 @@
 while [[ '$(kaggle kernels status morizin/jigsaw-wheels | egrep -o "[A-Z]{2,}")' == "RUNNING" ]]; do
     pass
 done
+
+pip install --no-index --find-links=wheels/ -r requirements.txt
+
