e4b658404b30b13d79707ab3c49b37a37b7498cd
Mohammed Rizin <mrizin2013@gmail.com>
2025-11-18 23:30:11 +0530

    Remove some files

--- scripts/notebook.sh
+++
@@ -1,5 +0,0 @@
-
-if [ ! -f "./working/kernel-metadata.json" ]; then
-    kaggle kernels init -p ./working/
-    if [[ "$(uname)" == "Darwin" ]]; then
-        sed -i "" "s/" ./working/kernel-metadata.json
