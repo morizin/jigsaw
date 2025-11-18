3df3f06542e132c2d98e281d42237c373833f126
Mohammed Rizin <mrizin2013@gmail.com>
2025-11-18 22:56:51 +0530

    Utility Notebook workflow

--- notebooks/installation/install.py
+++ notebooks/installation/install.py
@@ -1,3 +1,6 @@
 import os
 
 os.system("pip3 install -r /kaggle/input/jigsaw/requirements.txt -t /kaggle/working/")
+os.system(
+    "pip3 install --no-deps -t /kaggle/working /kaggle/input/jigsaw/jigsaw-0.1.*-py3-none-any.whl"
+)
